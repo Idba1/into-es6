5d71bfee74a1da74c3dddcb361769c866cbbd993
Idba1 <moniraislamself@gmail.com>
2024-02-21 21:48:07 +0600

    arrow function

--- arrow.js
+++ arrow.js
@@ -1,3 +1,27 @@
+//Normal function 
+
+function sat(a,b){
+    sum = a+b;
+    return sum;
+}
+
+sat(3,4);
+console.log(sum);
+
+
+// function expression 
+const sun = function(a,b){
+    return a+b;
+}
+
+
+
+// arrow function
+const mon = (a,b) => a*b;
+const multi = mon(3,4);
+console.log(multi);
+
+
 // function declaration 
 
 // function add (a, b){
@@ -21,5 +45,5 @@ const multiply = (num1, num2) => num1 * num2;
 // const sum = add3(5, 93);
 // const sum = add4(4,5,6,7)
 // console.log(sum);
-const mult = multiply(3, 12)
-console.log(mult)
+const mult = multiply(3, 12);
+console.log(mult);
