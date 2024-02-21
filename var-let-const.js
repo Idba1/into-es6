907f85b9cdcfd786c580081746fb96342658470a
Idba1 <moniraislamself@gmail.com>
2024-02-21 19:55:27 +0600

    val-let-const

--- var-let-const.js
+++ var-let-const.js
@@ -12,3 +12,18 @@ for(count ; count<=10; count++){
 count = 50; //let : allow it to reassign
 console.log(count);
 // let count = 0;
+
+
+/* for(let i = 0 ; i<=20; i++){
+    const num = 30;
+    total = num + i;
+    console.log(total);
+} */
+
+
+let sum = 0;
+for(let i = 0; i<=10 ; i++){
+    const num = i;
+    sum = sum + i ;
+    console.log(sum);
+}
